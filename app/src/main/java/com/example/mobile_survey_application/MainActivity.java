f62c25eb780031c99f835f0fd20967d0fe8bb661
rladl <rladlstjd87@gmail.com>
2026-04-30 13:28:48 +0900

    김인성-Main, Home, Survey Activity 수정됨

--- app/src/main/java/com/example/mobile_survey_application/MainActivity.java
+++ app/src/main/java/com/example/mobile_survey_application/MainActivity.java
@@ -16,12 +16,11 @@ import viewmodel.survey_viewmodel;
 
 public class MainActivity extends AppCompatActivity {
 
-    private void startApplication(){
-        Intent intent = new Intent(MainActivity.this, HomeActivity.class);
-        startActivity(intent);
+    private short autoDelayTime = 5000;
 
-        finish();
-    }
+    private Handler autoHomeHandler = new Handler();
+    private Runnable runable_delay = this::startApplication;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,13 +37,32 @@ public class MainActivity extends AppCompatActivity {
 
         // >> 화면을 클릭하거나 가만히 5초간 있으면 다음화면으로 넘어감, 이때 로그인 정보가 있다면 Home으로 그렇지 않으면 튜토리얼로
         LinearLayout root = findViewById(R.id.root_layout);
+
+        // 클릭 시 넘어가는 기능
         root.setOnClickListener(v -> {
             v.setEnabled(false); // 연타방지
-
             startApplication();
         });
-        new Handler().postDelayed(() -> {
-            startApplication();
-        }, 5000);
+
+        // 가만히 있으면 넘어가는 기능
+        autoHomeHandler.postDelayed(runable_delay, autoDelayTime);
     }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        autoHomeHandler.removeCallbacks(runable_delay);
+    }
+
+    // Application 처음 시작 시 Home으로 이동하는 함수
+    private void startApplication(){
+        autoHomeHandler.removeCallbacks(runable_delay);
+
+        Intent intent = new Intent(MainActivity.this, HomeActivity.class);
+        startActivity(intent);
+
+        finish();
+    }
+
+
 }
